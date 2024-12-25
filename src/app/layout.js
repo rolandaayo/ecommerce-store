479c0ba294d5e9eb75614c3a492eed0c891f7239
Roland Ibiwoye <contact@rolandayo.com>
2024-12-25 07:54:43 +0100

    framer-motion

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from 'react-hot-toast';
-import { CartProvider } from './context/CartContext'
+import { Toaster } from "react-hot-toast";
+import { CartProvider } from "./context/CartContext";
+import { AnimatePresence } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Oxtoyin | Clothing Store",
-  description: "Oxtoyin is a clothing store that sells a wide range of products for men, women, and children.",
+  description:
+    "Oxtoyin is a clothing store that sells a wide range of products for men, women, and children.",
 };
 
 export default function RootLayout({ children }) {
@@ -15,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.className}>
       <body>
         <CartProvider>
-          {children}
+          <AnimatePresence mode="wait">{children}</AnimatePresence>
         </CartProvider>
         <Toaster />
       </body>
